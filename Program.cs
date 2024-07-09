35fada75445c62cc4d9dde6f72410c90f15fc13f
F000NKKK <burtelgamerpro@gmail.com>
2024-07-09 10:31:17 +0600

    Update NLogger

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
 using PreProcessor.RabbitMq;
 
 namespace PreProcessor
@@ -10,20 +13,28 @@ namespace PreProcessor
         {
             var builder = Host.CreateApplicationBuilder(args);
 
+            // Настройка сервиса Windows, если приложение будет работать как Windows Service
             builder.Services.AddWindowsService(options =>
             {
                 options.ServiceName = ".NET RabbitMq Service";
             });
 
-            // Register RabbitMqService and the background service
+            // Регистрация RabbitMqService и фонового сервиса
             builder.Services.AddScoped<IRabbitMqService, PreProcessorBackgroundService>();
             builder.Services.AddScoped<IRabbitMqBackgroundService, PreProcessorBackgroundService>();
             builder.Services.AddHostedService<PreProcessorBackgroundService>();
 
+            // Настройка NLog
+            builder.Logging.ClearProviders();
+            builder.Logging.AddNLog("nlog.config");
+
             var app = builder.Build();
 
+            // Запуск приложения
             app.Run();
+
+            // Завершение записи логов перед закрытием приложения
+            LogManager.Shutdown();
         }
     }
 }
-
--- RabbitMq/RabbitMqBackgroundService.cs
+++ RabbitMq/RabbitMqBackgroundService.cs
@@ -3,11 +3,13 @@ using System.Text.Json;
 using Microsoft.Extensions.Hosting;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using NLog;
 
 namespace PreProcessor.RabbitMq
 {
     public class PreProcessorBackgroundService : BackgroundService, IRabbitMqService, IRabbitMqBackgroundService
     {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
         private readonly IConnection _connection;
         private readonly IModel _channel;
 
@@ -23,7 +25,7 @@ namespace PreProcessor.RabbitMq
             _channel.QueueDeclare(queue: ProcessorQueueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
             _channel.QueueDeclare(queue: FromWebCompBot, durable: true, exclusive: false, autoDelete: false, arguments: null);
 
-            // Установка ограничения на количество непотвержденных сообщений
+            // Установка ограничения на количество неподтверждённых сообщений
             _channel.BasicQos(0, 1, false);
         }
 
@@ -31,7 +33,7 @@ namespace PreProcessor.RabbitMq
         {
             var body = Encoding.UTF8.GetBytes(message);
             _channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
-            Console.WriteLine($"[x] Отправлено {message} в {queueName}");
+            Logger.Info($"Сообщение отправлено в {queueName}");
         }
 
         public void AcknowledgeMessage(ulong deliveryTag)
@@ -59,24 +61,26 @@ namespace PreProcessor.RabbitMq
                 var message = Encoding.UTF8.GetString(body);
                 var deserializedMessage = JsonSerializer.Deserialize<Message>(message);
 
-                Console.WriteLine($"Получено сообщение: {message}");
+                var flag = (deserializedMessage.Content != null) ? "NotNull" : "Null";
+                Logger.Info($"\nПолучено сообщение:\nId: {deserializedMessage.Id}\nContent: {flag}\nMessageCurrentTime: {deserializedMessage.MessageCurrentTime}");
 
                 try
                 {
                     // Обработка сообщения
-                    Console.WriteLine($"Обработка сообщения с ID: {deserializedMessage.Id}");
+                    Logger.Info($"Обработка сообщения с ID: {deserializedMessage.Id}");
 
-                    deserializedMessage.Content = deserializedMessage.Content.ToLower();
+                    deserializedMessage.Id += "#" + (Convert.ToInt32(deserializedMessage.Id.Split("$")[0]) + 1).ToString() + "$" + "Bot";
+                    deserializedMessage.IsUserMessage = false;
 
                     SendMessageToQueue(ProcessorQueueName, JsonSerializer.Serialize(deserializedMessage));
 
                     // Подтверждение сообщения
                     AcknowledgeMessage(ea.DeliveryTag);
-                    Console.WriteLine($"Сообщение подтверждено с deliveryTag: {ea.DeliveryTag}");
+                    Logger.Info($"Сообщение подтверждено с deliveryTag: {ea.DeliveryTag}");
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine($"Произошла ошибка: {ex.Message}");
+                    Logger.Error($"Произошла ошибка: {ex.Message}");
                     // Отклонение сообщения в случае ошибки
                     RejectMessage(ea.DeliveryTag, true);
                 }
@@ -90,17 +94,28 @@ namespace PreProcessor.RabbitMq
 
         public override void Dispose()
         {
-            _channel?.Close();
-            _connection?.Close();
-            base.Dispose();
+            try
+            {
+                _channel?.Close();
+                _connection?.Close();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Ошибка при закрытии соединения: {ex.Message}");
+            }
+            finally
+            {
+                base.Dispose();
+            }
         }
 
-        private class Message
+        // Класс для представления сообщения.
+        public class Message
         {
-            public string Id { get; set; } = string.Empty;
-            public string Content { get; set; } = string.Empty;
-            public string AnswerContent { get; set; } = string.Empty;
+            public string Id { get; set; } = string.Empty; // Идентификатор сообщения
+            public string Content { get; set; } = string.Empty; // Содержимое сообщения
+            public string MessageCurrentTime { get; set; } = string.Empty; // Время отправки сообщения
+            public Boolean IsUserMessage { get; set; } = true; // Флаг User/Bot, True/False соответственно
         }
     }
-
 }
